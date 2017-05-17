4564b8f12b2c7a29fb80767179797bb6ad79367d
itsquall <sergioacaraballo@gmail.com>
2017-05-17 10:15:34 +0200

    Add timestamp function for migration files

--- lib/mix/tasks/taglet/install.ex
+++ lib/mix/tasks/taglet/install.ex
@@ -48,5 +48,11 @@ defmodule Mix.Tasks.Taglet.Install do
     """
   end
 
-  defp timestamp, do: :os.system_time(:seconds)
+  defp timestamp do
+    {{y, m, d}, {hh, mm, ss}} = :calendar.universal_time()
+    "#{y}#{pad(m)}#{pad(d)}#{pad(hh)}#{pad(mm)}#{pad(ss)}"
+  end
+
+  defp pad(i) when i < 10, do: << ?0, ?0 + i >>
+  defp pad(i), do: to_string(i)
 end
