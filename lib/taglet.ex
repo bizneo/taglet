2c045de2c995ee4cb2b0c569427784dc7dc185a7
itsquall <sergioacaraballo@gmail.com>
2017-06-05 10:05:36 +0200

    Add add/3 and tag_list/2 functions

--- lib/taglet.ex
+++ lib/taglet.ex
@@ -2,17 +2,52 @@ defmodule Taglet do
   @moduledoc """
   Documentation for Taglet.
   """
+  import Ecto.Query
+  alias Taglet.{Tagging, Tag}
+
+  @repo Taglet.RepoClient.repo
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Taglet.hello
-      :world
-
   """
-  def hello do
-    :world
+  def add(struct, tag, context \\ "tag") do
+    tag_list = tag_list(struct, context)
+
+    case tag in tag_list do
+      true  -> tag_list
+      false ->
+        tag_resource = get_or_create(tag)
+
+        @repo.insert!(%Tagging{
+          taggable_id: struct.id,
+          taggable_type: struct.__struct__ |> Module.split |> List.last,
+          context: context,
+          tag_id: tag_resource.id
+        })
+
+        List.insert_at(tag_list, -1, tag)
+    end
+  end
+
+  defp get_or_create(tag) do
+    case @repo.get_by(Tag, name: tag) do
+      nil -> @repo.insert!(%Tag{name: tag})
+      tag_resource -> tag_resource
+    end
+  end
+
+  def tag_list(struct, context \\ "tag") do
+    id = struct.id
+    type = struct.__struct__ |> Module.split |> List.last
+
+    Tag
+    |> join(:inner, [t], tg in Tagging, t.id == tg.tag_id)
+    |> where([t, tg],
+      tg.context == ^context
+      and
+      tg.taggable_id == ^id
+    )
+    |> order_by([t, tg], asc: tg.inserted_at)
+    |> @repo.all
+    |> Enum.map(fn(result) -> result.name end)
   end
 end
--- test/taglet_test.exs
+++ test/taglet_test.exs
@@ -1,8 +1,62 @@
 defmodule TagletTest do
   use ExUnit.Case
+  import Ecto.Query
+
+  alias TagletPost, as: Post
+  alias Taglet.{Tagging, Tag}
+
+  @repo Taglet.RepoClient.repo
+
   doctest Taglet
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  setup do
+    @repo.delete_all(Post)
+    @repo.delete_all(Tagging)
+    @repo.delete_all(Tag)
+    on_exit fn ->
+      @repo.delete_all(Post)
+      @repo.delete_all(Tagging)
+      @repo.delete_all(Tag)
+    end
+    :ok
+  end
+
+  test "add/3 returns a list of associated tags with the new tag" do
+    post = @repo.insert!(%Post{title: "hello world"})
+    Taglet.add(post, "mytag")
+
+    result = Taglet.add(post, "mytag2")
+
+    assert result == ["mytag", "mytag2"]
+  end
+
+  test "add/3 with context returns a diferent list for every context" do
+    post = @repo.insert!(%Post{title: "hello world"})
+
+    result1 = Taglet.add(post, "mytag1", "context1")
+    result2 = Taglet.add(post, "mytag2", "context2")
+
+    assert result1 == ["mytag1"]
+    assert result2 == ["mytag2"]
+  end
+
+  test "tag_list/2 returns a list of associated tags" do
+    post = @repo.insert!(%Post{title: "hello world"})
+    Taglet.add(post, "mytag")
+    Taglet.add(post, "mytag2")
+
+    result = Taglet.tag_list(post)
+
+    assert result == ["mytag", "mytag2"]
+  end
+
+  test "tag_list/2 returns a list of associated tags for a specific context" do
+    post = @repo.insert!(%Post{title: "hello world"})
+    Taglet.add(post, "mytag", "context")
+    Taglet.add(post, "mytag2", "context")
+
+    result = Taglet.tag_list(post, "context")
+
+    assert result == ["mytag", "mytag2"]
   end
 end
