675ada15a5970f3a401ed25f49467dd784bb47bf
Moises <loldlm1@gmail.com>
2018-12-02 21:12:55 -0400

    fix logic

--- lib/taglet/tagging.ex
+++ lib/taglet/tagging.ex
@@ -2,7 +2,7 @@ defmodule Taglet.Tagging do
   use Ecto.Schema
   import Ecto.Changeset
 
-  @taggable_id_type if Application.get_env(:taglet, :taggable_id, :integer) == :uuid, do: :id
+  @taggable_id_type if Application.get_env(:taglet, :taggable_id) == :uuid, do: :id, else: :integer
 
   schema "taggings" do
     field :taggable_id, @taggable_id_type, null: false
